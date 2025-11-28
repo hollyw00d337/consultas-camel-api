d84d50925d048d2a2260454a65eabf566dfb3a5b
hollyw00d337 <121274226+hollyw00d337@users.noreply.github.com>
2025-11-28 17:39:04 +0000

    arreglar identacion en un getter

--- src/main/java/com/proyecto/consultas/model/Automovil.java
+++ src/main/java/com/proyecto/consultas/model/Automovil.java
@@ -23,7 +23,6 @@ public class Automovil {
         this.color = color;
     }
 
-    // --- GETTERS Y SETTERS (Obligatorios para JSON) ---
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
